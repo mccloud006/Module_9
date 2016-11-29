dccc852aaaba416e3c195dad5e7e6840e4a4f567
cmccloud <cmccloud@MCCLOUD-WIN8>
2016-11-29 17:15:24 -0600

    Add project files.

---
+++ Mod_9_Homework/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public Stack<double> grades;
+        public static int StudentCount = 0;
+
+        public Student(string firstName, string lastName, DateTime dob)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Dob = dob;
+            this.grades = new Stack<double>();
+        }              
+    }
+}
